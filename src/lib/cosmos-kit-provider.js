3f5a23afe62a045d8fbadde5a299f904fa6ae45a
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-18 16:23:25 +0100

    Update cosmos-kit-provider.js

--- src/lib/cosmos-kit-provider.js
+++ src/lib/cosmos-kit-provider.js
@@ -3,16 +3,31 @@
 import "@interchain-ui/react/styles";
 import { ChainProvider } from "@cosmos-kit/react";
 import { chains, assets } from "chain-registry";
-//import { wallets } from "@cosmos-kit/keplr";
-import { wallets } from 'cosmos-kit';
+import { wallets } from "cosmos-kit";
+import { GasPrice } from "@cosmjs/stargate";
 
 export function CosmosKitProvider({ children }) {
+  const chain = chains.find((c) => c.chain_name === "neutrontestnet");
+  const asset = assets.find((a) => a.chain_name === "neutrontestnet");
+
+  if (!chain || !asset) {
+    console.error("Chain or asset data not found for 'neutrontestnet'");
+    return null; // Prevent rendering if data is missing
+  }
+
   return (
     <ChainProvider
-      chains={chains}
-      assetLists={assets}
+      chains={[chain]} // Pass as array
+      assetLists={[asset]} // Pass as array
       wallets={wallets}
-      logLevel="DEBUG"
+      signerOptions={{
+        signingCosmwasm: {
+          gasPrice: GasPrice.fromString("0.01untrn"),
+        },
+        signingStargate: {
+          gasPrice: GasPrice.fromString("0.01untrn"),
+        },
+      }}
     >
       {children}
     </ChainProvider>
