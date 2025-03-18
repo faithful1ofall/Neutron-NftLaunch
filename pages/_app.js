9a302ec9a18d4c4a851d4ebc2e0051ffe2ce04cd
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-18 16:28:36 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,56 +1,23 @@
 import { useEffect, useState } from "react";
-import ContextProvider from "src/utils/ContextProvider";
 import GlobalStyles from "@assets/styles/GlobalStyles";
-//import { CosmosKitProvider } from "src/lib/cosmos-kit-provider";
-import { KeplrExtensionProvider, MetamaskExtensionProvider, KeplrMobileProvider, MetamaskMobileProvider, ShuttleProvider } from "@delphi-labs/shuttle-react"
-import { INJECTIVE_MAINNET,  INJECTIVE_TESTNET } from 'src/lib/chains';
-
-//import { wallets } from "@cosmos-kit/keplr";
-//import { wallets } from 'cosmos-kit';
+import { CosmosKitProvider } from "src/lib/cosmos-kit-provider";
 
 const App = ({ Component, pageProps }) => {
-  const [showChild, setShowChild] = useState(false);
-
-  const extensionProviders = [
-  new KeplrExtensionProvider({
-    networks: [INJECTIVE_MAINNET,  INJECTIVE_TESTNET]
-  }),
-  new MetamaskExtensionProvider({
-    networks: [INJECTIVE_MAINNET,  INJECTIVE_TESTNET]
-  })
-];
-
-  const mobileProviders = [
-  new KeplrMobileProvider({
-    networks: [INJECTIVE_MAINNET, INJECTIVE_TESTNET],
-    walletConnectProjectId: 'eab00b05bc608b9bccd13ed423d190c3'
-  }),
-  new MetamaskMobileProvider({
-    networks: [INJECTIVE_MAINNET, INJECTIVE_TESTNET],
-    walletConnectProjectId: 'eab00b05bc608b9bccd13ed423d190c3'
-  })
-];
-
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setShowChild(true);
+    setMounted(true);
   }, []);
 
-  if (!showChild) {
-    return null;
-  }
+  if (!mounted) return null; // Avoids hydration mismatch errors
 
   return (
-    <ShuttleProvider
-      extensionProviders={extensionProviders}
-      mobileProviders={mobileProviders}
-      persistent
-      >
-    <ContextProvider>
+    <>
       <GlobalStyles />
-      <Component {...pageProps} />
-    </ContextProvider>
-  </ShuttleProvider>
+      <CosmosKitProvider>
+        <Component {...pageProps} />
+      </CosmosKitProvider>
+    </>
   );
 };
 
