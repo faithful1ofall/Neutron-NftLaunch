ec127e26784a967d6ec5caab77febebb09c41875
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-18 18:11:56 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import GlobalStyles from "@assets/styles/GlobalStyles";
 import { CosmosKitProvider } from "src/lib/cosmos-kit-provider";
+import ContextProvider from "src/utils/ContextProvider";
 
 const App = ({ Component, pageProps }) => {
   const [mounted, setMounted] = useState(false);
@@ -12,12 +13,13 @@ const App = ({ Component, pageProps }) => {
   if (!mounted) return null; // Avoids hydration mismatch errors
 
   return (
-    <>
-      <GlobalStyles />
       <CosmosKitProvider>
+
+    <ContextProvider>
+         <GlobalStyles />
         <Component {...pageProps} />
+    </ContextProvider>
       </CosmosKitProvider>
-    </>
   );
 };
 
