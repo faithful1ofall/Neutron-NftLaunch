2b9a319b1fad300056f3f7534fc5e4fa750efe1f
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2025-03-19 13:18:49 +0100

    Update CollectionData.js

--- src/lib/CollectionData.js
+++ src/lib/CollectionData.js
@@ -14,7 +14,7 @@ const TEST_ENDPOINTS = getNetworkEndpoints(TEST_NETWORK);
 
 console.log('endpoint', TEST_ENDPOINTS.grpc);
 
-const chainGrpcWasmApi1 = new ChainGrpcWasmApi(TEST_ENDPOINTS.grpc);
+const chainGrpcWasmApi1 = new ChainGrpcWasmApi('https://grpc-falcron.pion-1.ntrn.tech:80');
 
 
 const fetchAllCollections = async () => {
